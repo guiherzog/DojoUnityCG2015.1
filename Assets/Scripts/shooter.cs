ff05894796fdccd97c4e52e169da54975731879e
Guilherme Herzog <guilhermeherzog@gmail.com>
2015-03-27 14:56:55 -0300

    - Commit Inicial com o Código até o Dojo02. - Remoção de Assets não utilizados. - Mudança em alguns parâmetros para deixar a jogabilidade melhor. ( Posição da Arma, da Camera e etc ). - Adição de Texturas com Mapa de Normal e Altura.

---
+++ Assets/Scripts/shooter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shooter : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public float Forca=10000.0f;
+	public float WaitTime=0.4f;
+
+	private float time;
+	// Use this for initialization
+	void Start () {
+		time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaTime = Time.time - time;
+
+		if (Input.GetAxis("Fire1") >0  && deltaTime > WaitTime) {
+			GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+			bullet.transform.position = this.transform.position;
+			bullet.rigidbody.AddForce(this.transform.forward*Forca);
+			time=Time.time;
+
+			audio.Play();		
+		}
+	}
+}
